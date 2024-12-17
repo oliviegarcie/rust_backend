afef9acc520fb07899962ce88f08d1427cd93356
oliviegarcie <olivia.garcia9832@gmail.com>
2024-12-17 09:54:05 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,50 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "gXf0qGns8eDa",
+        "outputId": "e9efd623-b2e1-451c-eb3b-14bc0b631477"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Hello World\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(\"Hello World\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "pQjJF-ZH8i-L"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
